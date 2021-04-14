4d9cce6b84102150989ac6874a24e1bd5d108124
csofya <48058980+csofya@users.noreply.github.com>
2021-04-14 18:15:35 -0400

    Update Money_Program_Sofya.py

--- Money_Program_Sofya.py
+++ Money_Program_Sofya.py
@@ -2,10 +2,12 @@
 $0 - $99
 input: 32
 output: Thirty two dollars
-
 '''
+
+import math
+
 numberones = {
-    0: 'zero', 
+    # 0: 'zero', 
     1: 'one',
     2: 'two',
     3: 'three',
@@ -49,31 +51,63 @@ def from_mon_to_eng(mon):
     # validates if money is between $0 - $99
     if (mon < 0 or mon > 99):
         return False
-    
+    if (mon == 0):
+        return 'zero dollars'
+        
+    # first check if mon is a float or int
+    # if mon has cents in it...
+    if (isinstance(mon, int) == False):
+        cents = math.ceil((mon % 1) * 100)
+        # if (cents > )
+        if (cents > 9):
+            onescent = (cents % 10)
+            if (onescent == 0):
+                tenscent = math.ceil((cents - onescent) / 10)
+                tenscentstr = numbertens[tenscent]
+                totalcents = 'and ' + tenscentstr + ' cents'
+            else:
+                onescentstr = numberones[onescent]
+                tenscent = math.floor((cents - onescent) / 10)
+                tenscentstr = numbertens[tenscent]
+                totalcents = 'and ' + tenscentstr + ' ' + onescentstr + ' cents'
+        else:
+            onescent = math.floor(cents % 10)
+            onescentstr = numberones[onescent]
+            totalcents = 'and ' + onescentstr + ' cents'
+    else:
+        totalcents = ''
+    mon = mon - (mon % 1)
     # iterate through each number
     ones = mon % 10 
     
-    # get the value from ones dictionary
-    onestring = numberones[ones] 
+    if (ones == 0):
+        onestring = ''
+    else:
+        # get the value from ones dictionary
+        onestring = numberones[ones] 
     
     # get the tens digit through arithmetic
     tens = (mon - ones) / 10
 
     if tens in numbertens:
         tensstring = numbertens[tens] # last is 3, value is three
-        result = tensstring + ' ' + onestring + ' dollars'
+        result = tensstring + ' ' + onestring + ' dollars ' + totalcents
         
     # else if the tens digit is one, go the teens dicionary
     elif tens == 1:
         teenstring = teen[ones]
-        result = teenstring + ' dollars'
+        result = teenstring + ' dollars ' + totalcents 
     
     # if there is only one digit, just print the ones string
     else: 
-        result = onestring + ' dollars'
+        result = onestring + ' dollars ' + totalcents 
         
-    return result      
+    return result
+    
+print(from_mon_to_eng(91.92)) 
+print(from_mon_to_eng(82)) 
+print(from_mon_to_eng(13.02)) 
+print(from_mon_to_eng(80.76)) 
 
-        
-for i in range(0, 100):
-    print(from_mon_to_eng(i))
+# for i in range(0, 100):
+#     print(from_mon_to_eng(i))
